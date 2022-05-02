8d939b11733079511697a84885cd55a74791641e
Radoslav Mandev <radoslav.mandev99@gmail.com>
2022-05-02 19:30:16 +0200

    Added main logic

--- app/src/main/java/de/uni_passau/fim/pqsigningapp/MainActivity.java
+++ app/src/main/java/de/uni_passau/fim/pqsigningapp/MainActivity.java
@@ -1,6 +1,230 @@
 package de.uni_passau.fim.pqsigningapp;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Base64;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.liboqs.Signature;
+import com.example.liboqs.Sigs;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Save all algorithms, the chosen one, the cipher (signed message) and public key.
+     */
+    private String[] algorithms;
+    private Signature signature;
+    private byte[] cipher;
+    private byte[] publicKey;
+
+    private EditText plainTextToSign;
+
+    /**
+     * Initialize the app. This includes fetching all available algorithms, view and
+     * button listeners.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Sign and validate example");
+
+        algorithms = getAllWantedAlgs();
+        initView();
+        initButtons();
+    }
+
+    /**
+     * Fetches all available algorithms, filtered so only the three finalists are retrieved.
+     *
+     * @return The wanted algorithms.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N) // not so important for now
+    private String[] getAllWantedAlgs() {
+        List<String> algs = Sigs.get_enabled_sigs();
+        algs = algs.stream()
+                .filter(e -> e.toLowerCase().contains("dilithium")
+                        || e.toLowerCase().contains("rainbow")
+                        || e.toLowerCase().contains("falcon"))
+                .collect(Collectors.toList());
+        return algs.toArray(new String[0]);
+    }
+
+    /**
+     * Initializes the dropdown with the algorithms and creates a listener for it.
+     */
+    private void initView() {
+        Spinner dropdown = findViewById(R.id.supportedAlgorithms);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, algorithms);
+        dropdown.setAdapter(adapter);
+        dropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                selectSigner(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+    }
+
+    /**
+     * Selects the chosen algorithm, creates the signature and public key.
+     * Then the information is set and the text is signed.
+     *
+     * @param chosenAlgorithmIndex The index from the dropdown.
+     */
+    private void selectSigner(final int chosenAlgorithmIndex) {
+        String chosenAlgorithm = algorithms[chosenAlgorithmIndex];
+        signature = new Signature(chosenAlgorithm);
+        publicKey = signature.generate_keypair();
+        setTextViews(signature.get_details());
+        handleTextToSign();
+    }
+
+    /**
+     * Sets the information about the chosen algorithm.
+     *
+     * @param algorithm The chosen algorithm.
+     */
+    @SuppressLint("SetTextI18n")
+    private void setTextViews(final List<String> algorithm) {
+        TextView algorithmName = findViewById(R.id.algorithmName);
+        TextView algorithmVersion = findViewById(R.id.algorithmVersion);
+        TextView nistLevel = findViewById(R.id.nistLevel);
+        TextView indCCA = findViewById(R.id.indCCA);
+        TextView publicKey = findViewById(R.id.publicKey);
+        TextView privateKey = findViewById(R.id.privateKey);
+        TextView lengthSignature = findViewById(R.id.lengthSignature);
+
+        algorithmName.setText("Chosen algorithm: " + algorithm.get(0));
+        algorithmVersion.setText("Version: " + algorithm.get(1));
+        nistLevel.setText("NIST level: " + algorithm.get(2));
+        indCCA.setText("Is IND-CCA: " + algorithm.get(3));
+        publicKey.setText("Length public key (bytes): " + algorithm.get(4));
+        privateKey.setText("Length private key (bytes): " + algorithm.get(5));
+        lengthSignature.setText("Maximum length signature (bytes): " + algorithm.get(6));
+    }
+
+    /**
+     * Call the plain text to be signed, by attaching a listener.
+     */
+    private void handleTextToSign() {
+        plainTextToSign = findViewById(R.id.document);
+        if (!plainTextToSign.getText().toString().equals("")) {
+            sign();
+        }
+        plainTextToSign.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                sign();
+            }
+        });
+    }
+
+    /**
+     * Signs the plaintext, assigns it and updates the text field that shows the cipher in UTF-8.
+     */
+    private void sign() {
+        plainTextToSign = findViewById(R.id.document);
+        cipher = signature.sign(plainTextToSign.getText().toString().getBytes());
+        updateCipherTextField();
+    }
+
+    /**
+     * Update the cipher upon change.
+     */
+    private void updateCipherTextField() {
+        TextView signedDocument = findViewById(R.id.signedDocument);
+        signedDocument.setText(
+                new String(Base64.encode(cipher, Base64.DEFAULT), StandardCharsets.UTF_8));
+    }
+
+    /**
+     * Assigns a listener for every button.
+     * The first only validates the signature, the second one shuffles the bytes inside and tries
+     * to validate it (in order to trigger a false signature) and the third one deletes the last bit
+     * in order to trigger a RuntimeException.
+     */
+    private void initButtons() {
+        Button validate = (Button) findViewById(R.id.validate);
+        Button shuffle = (Button) findViewById(R.id.shuffle);
+        Button alter = (Button) findViewById(R.id.alter);
+
+        validate.setOnClickListener(view -> verifyCipher());
+        shuffle.setOnClickListener(view -> {
+            shuffleCipher();
+            updateCipherTextField();
+            verifyCipher();
+        });
+        alter.setOnClickListener(view -> {
+            cipher = Arrays.copyOf(cipher, cipher.length + 1);
+            updateCipherTextField();
+            verifyCipher();
+        });
+    }
+
+    /**
+     * Just a typical shuffle of an array.
+     */
+    private void shuffleCipher() {
+        Random rnd = new Random();
+        for (int i = cipher.length - 1; i > 0; i--) {
+            int index = rnd.nextInt(i + 1);
+            byte a = cipher[index];
+            cipher[index] = cipher[i];
+            cipher[i] = a;
+        }
+    }
+
+    /**
+     * Tries to verify the cipher with the plain text message, cipher and public key.
+     */
+    private void verifyCipher() {
+        byte[] message = plainTextToSign.getText().toString().getBytes();
+        TextView isValidated = findViewById(R.id.isValidated);
+        if (cipher != null) {
+            String output;
+            try {
+                boolean verified = signature.verify(message, cipher, publicKey);
+                output = verified ? "Signature is valid."
+                        : "Signature is not valid, but key and signature length are right.";
+            } catch (RuntimeException e) {
+                output = "Signature is not valid. " + e.getMessage()
+                        + " (RuntimeException was thrown).";
+            }
+            isValidated.setText(output);
+        }
+    }
 }
